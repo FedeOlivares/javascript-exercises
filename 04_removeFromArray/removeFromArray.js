8be33706cdf9919b1b6922053fd736a105145ce6
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-09-18 17:43:41 -0300

    Solve TDD excercises 1 through 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let elements = arguments[0];
+    let toRemove = [];
+    let output = [];
+    for (let i = 1; i < arguments.length; i++) {
+        toRemove.push(arguments[i]);
+    }
+    for (let element of elements) {
+        if (!toRemove.includes(element)) {
+                output.push(element);
+        }
+    }
+    return output;
+    
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let output = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b) || ((a < 0) || (b < 0))){ // verifies no input is non integer and that no input is under 0
+        output = "ERROR";
+    } else {
+        if (a > b) {
+            let temp = a;
+            a = b; 
+            b = temp;
+        }
+        for (let i = a; i <= b; i++) {
+            output += i;
+        }
+    }
+    return output
 };
 
 // Do not edit below this line
