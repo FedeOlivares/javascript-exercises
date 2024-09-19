0b9fa62201ba5c4e10209b5105aaf7256dcc70b8
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-09-19 11:23:46 -0300

    Solve 06 leap years with 100ms less run time

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisible4 = !(year % 4);
+    let divisible100 = !(year % 100);
+    let divisible400 = !(year % 400);
 
+    if ((divisible400) || (divisible4 && !divisible100)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
