f08c6d59999db4081acac45528038b222acb9df3
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-09-30 14:59:56 -0300

    Solve fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index);
+    if (index === 0) {
+        return 0;
+    } else if (index < 0){
+        return "OOPS";
+    } else {
+        let i = 1;
+        let x = 0;
+        let counter = 2;
+        let temp = 0;
+        for (i = 1; counter <= index; counter++) {
+            temp = i;
+            i += x;
+            x = temp;
+        }
+        
+        return i;
+    }
 };
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
